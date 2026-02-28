f5ef1086efb44ad123db7e153b344737531b99f3
Samya Arora <samya.arora_cs.aiml24@gla.ac.in>
2026-02-28 11:26:53 +0530

    Initialize LDR setup with Serial communication

--- src/main.cpp
+++ src/main.cpp
@@ -21,4 +21,9 @@
  * - Sets LDR pin as INPUT
  * - Prints setup confirmation message
  */
+void setup() {
+    Serial.begin(9600);
+    pinMode(ldrpin, INPUT);
+    Serial.println("LDR setup");
+}
 
